0960a371572cf88ff0787fcdecf365dae8918cc7
Aman Adhikari <noattacklost@gmail.com>
2025-05-27 23:13:11 +0530

    "navbar done"

--- Frontend/src/components/Common/Header.jsx
+++ Frontend/src/components/Common/Header.jsx
@@ -1,13 +1,17 @@
 import Topbar from "../Layout/Topbar";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <div>
+    <header className="border-b border-gray-200">
+
        {/* Topbar */}
        <Topbar/>
        {/* navbar */}
+       <Navbar/>
+
        {/* Cart Drawer*/}
-    </div>
+    </header>
   );
 };
 
